c9cec90a3fba33088f00006be9bd3352c4695e05
unknown <CLNN-git>
2021-04-02 16:05:37 +0800

    v2

--- start_eth.sh
+++ start_eth.sh
@@ -1,1 +1,1 @@
-./nbminer -a ethash -o stratum+tcp://47.108.142.254:6688 -u 0x79103afa28d1d130b528164438d1acf20ec1f805.default
+./nbminer -a ethash -o stratum+tcp://47.108.142.254:6688 -u 0x4ae1ccD4D7788E9c5f0534231eD1b1d956701396.default
