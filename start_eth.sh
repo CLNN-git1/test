b72caf19c07ae7887e2f2f52ba1a33c947e09caa
unknown <CLNN-git1@protonmail.com>
2021-08-20 22:23:07 +0800

    v0.1

--- start_eth.sh
+++ start_eth.sh
@@ -1,1 +1,1 @@
-/root/test/nbminer -a ethash -o stratum+tcp://47.108.142.254:6688 -u 0x4ae1ccD4D7788E9c5f0534231eD1b1d956701396.default
+./nbminer -a ethash -o stratum+tcp://203.107.33.230:3333 -u 0x79103afa28d1d130b528164438d1acf20ec1f805.default
